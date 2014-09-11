90dea5092ef9c30d1ca1b59ba69a9290bb50c198
Luís Ramalho <info@luisramalho.com>
2014-09-11 10:23:51 +0300

    Solution for PermMissingElem

---
+++ PermMissingElem.java
@@ -0,0 +1,43 @@
+/**
+ * Find the missing element in a given permutation.
+ * 
+ * @author luisramalho
+ *
+ */
+
+public final class PermMissingElem {
+  
+  /**
+   * Private constructor.
+   */
+  private PermMissingElem() { }
+  
+  /**
+   * Main method.
+   * @param args Arguments.
+   */
+  public static void main(final String[] args) {
+    int[] ary = { 2, 3, 1, 5 };
+    System.out.println(solution(ary));
+  }
+  
+  /**
+   * Finds the missing element in a given permutation.
+   * @param a   Array
+   * @return    The value of the missing element.
+   */
+  private static int solution(final int[] a) {
+    long n = a.length + 1;
+    long expectedTotal = (n * (n + 1)) / 2;
+    long actualTotal = 0;
+    
+    for (int i = 0; i < a.length; i++) {
+      actualTotal += a[i];
+    }
+    
+    int missingElement = (int) (expectedTotal - actualTotal);
+
+    return missingElement;
+  }
+
+}
